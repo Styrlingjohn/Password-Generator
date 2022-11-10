43e1ddd28e2b8e9ba71143afee15cb98d47fb9b2
Styrlingjohn <styrling_morris@yahoo.com>
2022-11-10 11:17:33 -0700

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var chars = "123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 12;
+  var password = "";
+for (var i = 0; i <= passwordLength; i++){
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+}
+ document.querySelector("password").value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
