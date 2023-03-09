b60096563cfb96ef7c8514fc2626f8b5cfde7a1b
Styrlingjohn <styrling_morris@yahoo.com>
2023-03-09 11:41:32 -0700

    attempted to fix connection

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,61 @@
 // Assignment Code
-var resultEL = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers = "1234567890".split("");
+var symbols = "!@#$%^&*()".split("");
 
-
-
-// Random selctor function
+// Random selector function
 function generatePassword() {
-  let generatePassword = '';
+  var passwordFinal = '';
   let passwordLength = prompt("How many characters would you like your password to be?");
-  var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-  var numbers = "1234567890".split("");
-  var symbols = "!@#$%^&*()".split("");
   var useLowercase = confirm(`The use of lower case letters`)
   var useUppercase = confirm(`The use of upper case letters`)
   var useNumbers = confirm(`The use of numbers`)
   var useSymbols = confirm(`The use of symbols`)
-  var finalPassword = []
+  console.log(passwordLength);
+  console.log(lowercase);
+  console.log(useLowercase);
+
   if (passwordLength >= 8 && passwordLength <= 128) {
-    var tempPassword = []
     for (var i = 0; i < passwordLength; i++) {
-      if (useLowercase) {
-        tempPassword.push(lowercase[Math.floor(Math.random() * lowercase.length)])
+      // console.log(passwordLength);
+      if (useLowercase & passwordFinal.length < passwordLength) {
+        var randomArray= Math.floor(Math.random() * lowercase.length);
+        passwordFinal += lowercase[randomArray]
       }
-      if (useUppercase) {
-        tempPassword.push(uppercase[Math.floor(Math.random() * uppercase.length)])
+
+      if (useUppercase & passwordFinal.length < passwordLength) {
+        var randomArray = Math.floor(Math.random() * uppercase.length);
+        passwordFinal += uppercase[randomArray]
       }
-      if (useNumbers) {
-        tempPassword.push(numbers[Math.floor(Math.random() * numbers.length)])
+     
+      if (useNumbers & passwordFinal.length < passwordLength) {
+        var randomArray = Math.floor(Math.random() * numbers.length);
+        passwordFinal += numbers[randomArray]
       }
-      if (useSymbols) {
-        tempPassword.push(symbols[Math.floor(Math.random() * symbols.length)])
+
+      if (useSymbols & passwordFinal.length < passwordLength) {
+        var randomArray= Math.floor(Math.random() * symbols.length);
+        passwordFinal += symbols[randomArray]
       }
-      for (let i = 0; i <= passwordLength; i++)
-        finalPassword.push(tempPassword[Math.floor(Math.random() * tempPassword.length)])
     }
-    resultEL.value = finalPassword.splice(0,passwordLength).join("")
-  }
-}
+
+      return passwordFinal;
+      
+  }  
+  console.log(passwordFinal);
+
 
  
-generatePassword()
+}
+  //  console.log(passwordFinal);
+function writePassword() {
+  var generatedPassword = generatePassword();
+  var passwordText = document.querySelector("#password");
+  // console.log(passwordText);
+  passwordText.value = generatedPassword;
+
+}
+
+generateBtn.addEventListener("click", writePassword);
